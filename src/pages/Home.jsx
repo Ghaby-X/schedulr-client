06ddf34001ca99592bb1fa6b180717e15787afb0
Gabriel Anyaele <105595126+Ghaby-X@users.noreply.github.com>
2025-01-11 20:37:17 +0000

    Update Home.jsx

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,9 +1,16 @@
-import React from 'react'
+import React, { useEffect } from 'react'
+import { useNavigate } from 'react-router-dom';
 
 const Home = () => {
+    const navigate = useNavigate();
+
+    useEffect(() => {
+        navigate('/dashboard');
+    }, [navigate]);
+
     return (
         <div>Home</div>
-    )
+    );
 }
 
-export default Home
+export default Home;
